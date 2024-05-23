789f976e3cc374b900be94d5a8365bcc4e0a6ec3
William Boyer <w.boyer@outlook.com>
2024-05-23 20:23:04 +0100

    Fixed a bug where the word to guess would change partway through a game

--- milestone_4.py
+++ milestone_4.py
@@ -14,19 +14,22 @@ class Hangman:
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
-            for letter in self.word:
-                if letter == guess:
-                    index = self.word.index(letter)
-                    # Get corresponding blank in word_guessed
-                    self.word_guessed[index] = letter
+            # Finds which letters in word match the correct guess
+            # Then reveals the letters in word_guessed
+            guessed_index = [i for i in range(len(self.word)) if self.word[i] == guess]
+            for i in guessed_index:
+                self.word_guessed[i] = self.word[i]
             self.num_letters = self.num_letters - 1
+            str_word_guessed = ''.join(self.word_guessed)
+            print(f"Your answer so far: {str_word_guessed}")
         else:
             self.num_lives = self.num_lives - 1
             print(f"Sorry, {guess} is not in the word. Try again.")
             print(f"You have {self.num_lives} lives left.")
             
     def ask_for_input(self):
-        while(True):
+        is_valid = False
+        while(not is_valid):
             guess = input("Guess a letter.\n")
             if len(guess) != 1 and guess.isalpha()==False:
                 print("Invalid letter. Please, enter a single alphabetical character.")
@@ -35,8 +38,4 @@ class Hangman:
             else:
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
-                break
-            
-            
-hangman = Hangman(word_list=['apple', 'banana', 'apricot', 'pineapple', 'tomato'])
-hangman.ask_for_input()
+                is_valid = True
